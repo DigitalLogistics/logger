1560971b4e83efc53e0380a3bbff582f00791dc2
Oliver Charles <ollie@ocharles.org.uk>
2014-04-14 17:22:57 +0100

    monad-logger/default.nix

---
+++ monad-logger/default.nix
@@ -0,0 +1,20 @@
+{ cabal, blazeBuilder, conduit, exceptions, fastLogger, liftedBase
+, monadControl, monadLoops, mtl, resourcet, stm, stmChans, text
+, transformers, transformersBase
+}:
+
+cabal.mkDerivation (self: {
+  pname = "monad-logger-new";
+  version = "0.4";
+  src = ./.;
+  buildDepends = [
+    blazeBuilder conduit exceptions fastLogger liftedBase monadControl monadLoops
+    mtl resourcet stm stmChans text transformers transformersBase
+  ];
+  meta = {
+    homepage = "https://github.com/kazu-yamamoto/logger";
+    description = "A class of monads which can log messages";
+    license = self.stdenv.lib.licenses.mit;
+    platforms = self.ghc.meta.platforms;
+  };
+})
